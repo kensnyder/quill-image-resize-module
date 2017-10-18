4fdcd83b31ed75cc6fef2706e58a795bb8cf2be8
nmonterroso <nmonterroso@users.noreply.github.com>
2017-10-18 15:09:07 -0700

    exporting BaseModule

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export { default as IframeResize } from './iframe/IframeResize';
 export { default as ImageResize } from './image/ImageResize';
 export { default as AlignmentHelper } from './AlignmentHelper';
 export { default as DeleteHelper } from './DeleteHelper';
+export { BaseModule } from './image/modules/BaseModule';
 export { Toolbar } from './image/modules/Toolbar';
